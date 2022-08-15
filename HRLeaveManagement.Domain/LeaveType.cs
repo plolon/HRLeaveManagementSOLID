63d7c0a0794134aae027d2cbdd47406df7bfb2e4
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-15 13:21:39 +0200

    Created classLibrary project

---
+++ HRLeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HRLeaveManagement.Domain
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
