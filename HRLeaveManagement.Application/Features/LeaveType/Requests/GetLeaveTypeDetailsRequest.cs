99a451095530c3d77bffdb32fb5fa5d813000070
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-16 11:07:46 +0200

    Created getby id request handler for CQRS pattern

--- HRLeaveManagement.Application/Features/LeaveType/Requests/GetLeaveTypeDetailsRequest.cs
+++ HRLeaveManagement.Application/Features/LeaveType/Requests/GetLeaveTypeDetailsRequest.cs
@@ -1,11 +1,10 @@
 ﻿using HRLeaveManagement.Application.DTOs;
 using MediatR;
-using System.Collections.Generic;
 
 namespace HRLeaveManagement.Application.Features.LeaveType.Requests
 {
-    public class GetLeaveTypeListRequest :IRequest<List<LeaveTypeDto>>
+    public class GetLeaveTypeDetailsRequest : IRequest<LeaveTypeDto>
     {
-
+        public int Id { get; set; }
     }
 }
