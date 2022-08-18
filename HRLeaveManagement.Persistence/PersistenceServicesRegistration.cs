9ffe0f0207f66c8ca72c72c926bd42847235fd4b
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-18 13:52:03 +0200

    Added static class for configure persistenceServices

---
+++ HRLeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,23 @@
+﻿using HRLeaveManagement.Application.Persistence.Contracts;
+using HRLeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRLeaveManagement.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString")));
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+            return services;
+        }
+    }
+}
