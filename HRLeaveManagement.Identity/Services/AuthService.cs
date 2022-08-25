58e734418784e29da813c7199f34552a4363c2c2
Dawid Bańczak <dawid.banczak99@gmail.com>
2022-08-25 13:55:15 +0200

    Created JWT login/register services and created extension for implementation that services

---
+++ HRLeaveManagement.Identity/Services/AuthService.cs
@@ -0,0 +1,83 @@
+﻿using HRLeaveManagement.Application.Contracts.Identity;
+using HRLeaveManagement.Application.Models.Identity;
+using HRLeaveManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Identity.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly JwtSettings jwtSettings;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AuthService(UserManager<ApplicationUser> userManager,
+            IOptions<JwtSettings> jwtSettings,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.jwtSettings = jwtSettings.Value;
+            this.signInManager = signInManager;
+        }
+        public async Task<AuthResponse> Login(AuthRequest request)
+        {
+            var user = await userManager.FindByEmailAsync(request.Email);
+            if (user == null)
+                throw new Exception($"User with {request.Email} not found.");
+            var result = await signInManager.PasswordSignInAsync(user.UserName, request.Password, false, lockoutOnFailure: false);
+            if (!result.Succeeded)
+                throw new Exception($"Credentials for {request.Email} aren't valid.");
+
+            JwtSecurityToken jwtSecurityToken = await GenerateToken(user);
+
+            AuthResponse response = new AuthResponse
+            {
+                Id = user.Id,
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                Email = user.Email,
+                UserName = user.UserName
+            };
+
+            return response;
+        }
+
+        private async Task<JwtSecurityToken> GenerateToken(ApplicationUser user)
+        {
+            var userClaims = await userManager.GetClaimsAsync(user);
+            var roles = await userManager.GetRolesAsync(user);
+            var roleClaims = new List<Claim>();
+            roles.ToList().ForEach(x => roleClaims.Add(new Claim("roles", x)));
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            }.Union(userClaims).Union(roleClaims);
+            var symetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Key));
+            var signingCredentials = new SigningCredentials(symetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: jwtSettings.Issuer,
+                audience: jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(jwtSettings.DurationInMinutes),
+                signingCredentials: signingCredentials);
+            return jwtSecurityToken;
+        }
+
+        public Task<RegistrationResponse> Register(RegistrationRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
